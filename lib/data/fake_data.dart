0626ef65a4eed9581a95ae67ce21af0940caa264
iloneslife <jasony1@naver.com>
2021-12-14 13:56:46 +0900

    image seartch app

---
+++ lib/data/fake_data.dart
@@ -0,0 +1,14 @@
+import 'package:image_search/model/photo.dart';
+
+final fakePhotos = [
+  Photo(
+    previewURL: "https://cdn.pixabay.com/photo/2016/08/28/23/24/sunflower-1627193_150.jpg",
+  ),
+  Photo(
+    previewURL: "https://cdn.pixabay.com/photo/2018/11/03/21/42/sunflowers-3792914_150.jpg",
+  ),
+  Photo(
+    previewURL: "https://cdn.pixabay.com/photo/2018/01/28/11/24/sunflower-3113318_150.jpg",
+  ),
+
+];
---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:image_search/data/fake_data.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('이미지 검색'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: fakePhotos.map((e) => Image.network(e.previewURL)).toList(),
+      ),
+    );
+  }
+}
